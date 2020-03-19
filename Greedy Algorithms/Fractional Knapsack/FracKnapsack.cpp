b8080b087a281fe1a50c9492b6ab02b891c66a70
mayank992 <mayankjindal992@gmail.com>
2020-03-19 20:42:39 +0530

    Add Fractional Knapsack Problem

---
+++ Greedy Algorithms/Fractional Knapsack/FracKnapsack.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Item {
+    public: 
+        int value, weight;
+        double fraction;
+    
+        Item() {
+            value = weight = fraction = 0;
+        }
+};
+
+bool cmp(const Item& a, const Item& b) {
+    return a.fraction > b.fraction;
+}
+
+int main() {
+    int n, w, value, weight;
+    // n is total number of items & w is knapsack capacity
+    cin >> n >> w;
+    // array which stores value, weight, fraction of each item
+    Item *data = new Item[n];
+    // input
+    for(int i = 0; i < n; i++) {
+        cin >> value >> weight;
+        data[i].value = value;
+        data[i].weight = weight;
+        data[i].fraction = (double )value / weight;
+    }
+    // sort the data in non-decreasing order on the basis of the ratioof value/weight
+    sort(data, data + n, cmp);
+    int cur_weight = 0, final_value = 0;
+    for(int i = 0; i < n; i++) {
+        if(w - cur_weight >= data[i].weight) {
+            cur_weight += data[i].weight;
+            final_value += data[i].value;
+        }
+        else {
+            final_value += (w - cur_weight) * data[i].fraction;
+            break;
+        }
+        if(w == cur_weight)
+            break;
+    }
+    cout << "Maximum Value in knapsack: " << final_value << "\n";
+    return 0;
+}
